a9c4704c7cd9bed478a5e260484958150f2faa45
DavDeDev <pietrocoladavid@gmail.com>
2023-08-06 13:04:24 -0400

    feat: bottombar completed

--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -1,3 +1,42 @@
-export default function Bottombar(){
-    return <h2>BottomBar</h2>
+'use client'
+
+import { sidebarLinks } from "@/constants";
+import Link from "next/link";
+import Image from 'next/image';
+import { usePathname } from "next/navigation";
+
+export default function Bottombar() {
+
+    const pathname = usePathname();
+
+    return (
+        <section className="bottombar">
+            <div className="bottombar_container">
+                {
+                    sidebarLinks.map((link) => {
+
+                        const isActive = (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route;
+
+                        // We wrap this map into a function rather then instant returning so we can add some more logic
+                        return (
+                            <Link href={link.route}
+                                key={link.label}
+                                className={`bottombar_link ${isActive && 'bg-primary-500'}`}>
+                                <Image
+                                    src={link.imgURL}
+                                    alt={link.label}
+                                    width={24}
+                                    height={24}
+                                />
+                                <p className='text-subtle-medium text-light-1 max-sm:hidden'>
+                                    {link.label.split(/\s+/)[0]}
+                                </p>
+                            </Link>
+                        )
+                    })
+                }
+            </div>
+
+        </section>
+    )
 }
