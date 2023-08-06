81cecf6ce03292d0e9afa77892f3c8ef5006108b
DavDeDev <pietrocoladavid@gmail.com>
2023-08-05 22:51:46 -0400

    feat: Auth group layout

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,29 @@
+import { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+
+import '../global.css';
+
+export const metadata: Metadata = {
+    title: 'Threads',
+    description: 'A place to share your thoughts in Next.js 13',
+}
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout(
+    {
+        children
+    }
+        :
+        {
+            children: React.ReactNode
+        }) {
+    return (
+        <ClerkProvider>
+            <body className={`${inter.className} bg-dark-1 }>
+                {children}
+            </body>
+        </ClerkProvider>
+    )
+}
