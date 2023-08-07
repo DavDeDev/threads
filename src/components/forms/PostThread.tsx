c1b0e05e8597439d62258b598541565734c783e5
DavDeDev <pietrocoladavid@gmail.com>
2023-08-07 18:52:21 -0400

    feat: PostThread form

--- src/components/forms/PostThread.tsx
+++ src/components/forms/PostThread.tsx
@@ -1,3 +1,89 @@
-export default function PostThread ({userId}:{userId: string}){
-    return <h1>Post Thread form</h1>
+"use client";
+
+import * as z from "zod";
+import Image from "next/image";
+import { useForm } from "react-hook-form";
+import { usePathname, useRouter } from "next/navigation";
+import { ChangeEvent, useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+
+import { useUploadThing } from "@/lib/uploadthing";
+import { isBase64Image } from "@/lib/utils";
+
+import { ThreadValidation } from "@/lib/validations/thread";
+import { updateUser } from "@/lib/actions/user.actions";
+
+interface Props {
+    user: {
+        id: string;
+        objectId: string;
+        username: string;
+        name: string;
+        bio: string;
+        image: string;
+    };
+    btnTitle: string;
+}
+
+
+
+
+export default function PostThread({ userId }: { userId: string }) {
+    const router = useRouter();
+    const pathname = usePathname();
+
+    const form = useForm({
+        resolver: zodResolver(ThreadValidation),
+        defaultValues: {
+            thread: '',
+            accountId: userId,
+        },
+    });
+
+    const onSubmit = () => {
+        await
+    }
+
+
+    return (
+        <Form {...form}>
+            <form
+                className='flex flex-col justify-start gap-10'
+                onSubmit={form.handleSubmit(onSubmit)}
+            >
+                <FormField
+                    control={form.control}
+                    name='thread'
+                    render={({ field }) => (
+                        <FormItem className='flex w-full flex-col gap-3'>
+                            <FormLabel className='text-base-semibold text-light-2'>
+                                Content of the Thread
+                            </FormLabel>
+                            <FormControl className="no-focus border border-dark-5 bg-dark-3 text-light-1">
+                                <Textarea
+                                    rows={15}
+                                    className='account-form_input no-focus'
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit" className="bg-primary-500">Post Thread</Button>
+            </form>
+        </Form>
+    )
 }
